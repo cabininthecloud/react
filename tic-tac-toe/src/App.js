79ece688463364a97dc70fed952356c39a98c4d7
Eric Schmidt <cabininthecloud@users.noreply.github.com>
2024-05-30 19:25:23 -0700

    tic-tac-toe with improvements
    1. For the current move only, show “You are at move #…” instead of a button.
    2. Rewrite Board to use two loops to make the squares instead of hardcoding them.
     3. When no one wins, display a message about the result being a draw.

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -33,7 +33,13 @@ export default function Game() {
     }
     return (
       <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
+        {
+        (move === history.length-1)
+          ?
+            ('You are at move #' + move)
+          :
+            (<button onClick={() => jumpTo(move)}>{description}</button>)
+        }
       </li>
     );
   });
@@ -68,28 +74,26 @@ function Board({ xIsNext, squares, onPlay }) {
   let status;
   if (winner) {
     status = 'Winner: ' + winner;
+  } else if (!squares.includes(null)){
+    status = 'Draw';
   } else {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  const board = [];
+  for(let i=0; i<3; i++) {
+    const row = [];
+    for(let j=0; j<3; j++) {
+      let num = i*3 + j;
+      row.push(<Square key={num} value={squares[num]} onSquareClick={() => handleClick(num)} />)
+    }
+    board.push(<div className="board-row">{row}</div>)
   }
 
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      {board}
     </>
   );
 }
